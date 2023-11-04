eba6c3f9490b7b5eafb205666a50367970cad56e
physicsuofraui <kodyrogers21@gmail.com>
2023-11-03 19:09:09 -0600

    added very basic Arduino MKR 1010 code

---
+++ WiFiSimpleSender_mkr1010_temp_test/WiFiSimpleSender_mkr1010_temp_test.ino
@@ -0,0 +1,141 @@
+/*
+  ArduinoMqttClient - WiFi Simple Sender
+
+  This example connects to a MQTT broker and publishes a message to
+  a topic once a second.
+
+  The circuit:
+  - Arduino MKR 1000, MKR 1010 or Uno WiFi Rev2 board
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA)
+  #include <WiFi.h>
+#endif
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "test.mosquitto.org";
+int        port     = 1883;
+const char topic[]  = "Tempdata";
+
+const long interval = 300000;
+unsigned long previousMillis = 0;
+
+float SERIESRESISTOR = 9.87;
+
+float TEMPERATURENOMINAL = 25.0;
+
+float BCOEFFICIENT = 3540.0;
+
+float thermistor = 0.0;
+
+float THERMISTORNOMINAL = 10;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  // set analog resulotion to 12
+  analogReadResolution(12);
+}
+
+void loop() {
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // to avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+  // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    /*Read analog outputof NTC module,
+     i.e the voltage across the thermistor */
+    float average = ((float)analogRead(A0) / 4095.0) * 3.3;
+    
+    average = (average * SERIESRESISTOR) / (3.3 - average);
+    Serial.print("Thermistor resistance "); 
+    Serial.println(average);
+    
+    float steinhart;
+    steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = log(steinhart);                  // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart;                 // Invert
+    steinhart -= 273.15;                         // convert absolute temp to C
+    
+    Serial.print("Temperature "); 
+    Serial.print(steinhart);
+    Serial.println(" *C");
+    
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.print("The temperature is");
+    Serial.println(thermistor);
+
+    // send message, the Print interface can be used to set the message contents
+    mqttClient.beginMessage(topic);
+    mqttClient.print("The temperature is ");
+    mqttClient.print(steinhart);
+    mqttClient.endMessage();
+
+    Serial.println();
+  }
+}
---
+++ WiFiSimpleSender_mkr1010_temp_test/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "shop_and_yard"
+#define SECRET_PASS "12345678"
